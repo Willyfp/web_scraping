f1545728b37cd4e409eb0454ae8480ba2058712e
willy <willypereira41@gmail.com>
2022-09-23 20:18:51 -0300

    Salvar dados no banco e gerar coordenadas

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const EventEmitter = require("events");
+const nodeGeocoder = require("node-geocoder");
+
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+
+const { getFirestore } = require("firebase-admin/firestore");
+
+const serviceAccount = require("C:\\Users\\willy\\Downloads\\buscape-90a48-firebase-adminsdk-nmmin-173ced83df.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+  databaseURL: "https://buscape-90a48-default-rtdb.firebaseio.com",
+});
+
+const db = getFirestore();
 
 const server = express();
 
@@ -34,15 +52,70 @@ server.get("/", async (request, response) => {
     const newPage = await browser.newPage();
     await newPage.goto(String(item));
 
-    var title = await newPage.evaluate(() => {
-      return document.querySelector("div.header-title span").textContent;
+    const apartment = await newPage.evaluate(async () => {
+      const description = document.querySelectorAll("h1 span");
+      const info = document.querySelectorAll(".item-info");
+      const arrayImages = document.querySelectorAll(".slide-image img");
+      const prices = document.querySelectorAll(".knl_panels-list .tax");
+
+      const price = {};
+
+      prices.forEach((item) => {
+        const itemSplited = item.textContent.split("$ ");
+
+        price[itemSplited[0]] = itemSplited[itemSplited.length - 1];
+      });
+
+      const comodities = [];
+
+      info.forEach((item) => {
+        if (item?.textContent) {
+          comodities.push(item.textContent);
+        }
+      });
+
+      const images = [];
+
+      arrayImages.forEach((item) => {
+        if (item.src) {
+          images.push({ uri: item.src });
+        }
+      });
+
+      return {
+        description: description[0].textContent,
+        address: description[1]?.textContent,
+        comodities,
+        images,
+        price,
+      };
     });
 
-    newAppartments.push({ title });
+    if (apartment.address !== "") newAppartments.push(apartment);
+  }
+
+  const appartmentsFormated = [];
+
+  for (item of newAppartments) {
+    const res = await nodeGeocoder({
+      apiKey: "AIzaSyAJ9TKIMyEFz0YzZPCWwdTZxm6GaFIhipM",
+    }).geocode(item.address);
+
+    appartmentsFormated.push({
+      ...item,
+      coords: {
+        latitude: res[0].latitude,
+        longitude: res[0].longitude,
+      },
+    });
+  }
+
+  for (ap of appartmentsFormated) {
+    await db.collection("appartments").add(ap);
   }
 
   await browser.close();
-  response.send(newAppartments);
+  response.send(appartmentsFormated);
 });
 
 server.listen(3000);
